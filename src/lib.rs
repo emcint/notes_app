63d7ed86fbc04830e5211bf74702450b8e35ca49
Ethan McIntosh <128169416+emcint@users.noreply.github.com>
2023-04-07 18:13:46 +0100

    Fixed typo

---
+++ src/lib.rs
@@ -0,0 +1,300 @@
+pub mod root_init {
+
+}
+
+pub mod verify {
+
+    pub fn verify_password(user_password: String) -> bool {
+        let password = "password".to_string(); // load file todo!!!!!
+        if user_password == password {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+pub mod application_window {
+
+    use eframe::{self, run_native, App, NativeOptions, *};
+    use egui::{CentralPanel, SidePanel, TopBottomPanel, Ui, Window, Label, widgets};
+    use std::fs::*;
+    use std::io::{Read, Write};
+    use std::path::PathBuf;
+    use std::ptr::write_bytes;
+
+    use crate::verify;
+
+    pub struct ApplicationWindow {
+        user_input: String,
+        validity: bool,
+        attempts: u128,
+        show_incorrect: bool,
+        path: std::path::PathBuf,
+        show_new_folder: bool,
+        show_new_note: bool,
+        folder_name: String,
+        note_name: String,
+        show_sync_spinner: bool,
+        current_focus: std::path::PathBuf,
+        current_file_buffer: String,
+        input_cache: Vec<String>,
+    }
+
+    impl Default for ApplicationWindow {
+        fn default() -> Self {
+            Self {
+                user_input: "".to_string(), // used for the login screen
+                validity: false, // determines which state the app is in (login or main)
+                attempts: 0, // used to determine if the user has tried to login too many times
+                show_incorrect: false, // used to show the incorrect message on the login screen
+                path: std::path::PathBuf::from(String::from("src\\data")), // used to store the path to the data folder (needs to be changed to a config file)
+                show_new_folder: false, // used to show the new folder window
+                show_new_note: false, // used to show the new note window
+                folder_name: "".to_string(), // used to store the name of the new folder
+                note_name: "".to_string(), // used to store the name of the new note
+                show_sync_spinner: false, // used to show the sync spinner
+                current_focus: std::path::PathBuf::from(String::from("")), // used to store the path to the current file/note
+                current_file_buffer: String::new(), // used to store the contents of the current file/note
+                input_cache: Vec::new(), // todo!
+            }
+        }
+    }
+
+    impl ApplicationWindow {
+        pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+            cc.egui_ctx.set_visuals(egui::Visuals::dark());
+            Default::default()
+        }
+    }
+
+    impl App for ApplicationWindow {
+        fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+            TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+                ui.vertical(|ui| {
+
+                    ui.add_space(1.0);
+                    ui.horizontal(|ui: &mut Ui| {
+
+                        ui.hyperlink_to("Source code", "github.com");
+                        ui.add_space(5.0);
+                        ui.label("Version 0.1.0");
+                        ui.add_space(50.0);
+
+                        if self.validity {
+                            if ui.button("Sync").clicked() {
+                                self.show_sync_spinner = true;
+                                for i in 1..=3 {
+                                    println!("Syncing {}", i); // todo: replace with actual sync code
+                                }
+                            }
+                            if self.show_sync_spinner {
+                                ui.spinner();
+                            }
+                        }
+                        
+                    });
+                    ui.add_space(1.0);
+                });
+            });
+
+            TopBottomPanel::top("top_panel").show(ctx, |ui| {
+                ui.horizontal_centered(|ui: &mut Ui| {
+                    ui.label("Notes");
+                });
+            });
+
+            if self.validity == false {
+                CentralPanel::default().show(ctx, |ui| {
+                    ui.vertical_centered(|ui: &mut Ui| {
+                        ui.heading("Enter Password:");
+                        ui.add_space(5.0);
+                        let input_field = ui.text_edit_singleline(&mut self.user_input);
+                        if input_field.gained_focus() {
+                            self.show_incorrect = false;
+                        };
+                        ui.add_space(5.0);
+                        if input_field.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))
+                        {
+                            ui.add_space(1.0);
+                            ui.spinner();
+
+                            self.validity = verify::verify_password(self.user_input.clone());
+
+                            if self.validity == false {
+                                self.show_incorrect = true;
+                                self.attempts += 1;
+                            };
+                            /*
+                            if self.attempts >= 3 {
+                                ui.add_space(1.0);
+                                ui.label("Too many attempts");
+
+                                frame.close();
+                            };
+                            */
+                        };
+                        if self.show_incorrect == true {
+                            ui.label("Incorrect");
+                            ui.add_space(5.0);
+                        };
+                        if ui.button("Continue").clicked() {
+                            ui.add_space(1.0);
+                            ui.spinner();
+
+                            self.validity = verify::verify_password(self.user_input.clone());
+
+                            if self.validity == false {
+                                self.show_incorrect = true;
+                                self.attempts += 1;
+                            };
+                            /*
+                            if self.attempts >= 3 {
+                                ui.add_space(1.0);
+                                ui.label("Too many attempts");
+
+                                frame.close();
+                            };
+                            */
+                            ui.add_space(1.0);
+                        };
+                    });
+                });
+            } else {
+                SidePanel::left("left_panel").show(ctx, |ui| {
+                    ui.make_persistent_id("left_panel");
+                    self.persist_egui_memory();
+                    ui.add_space(5.0);
+                    ui.horizontal(|ui: &mut Ui| {
+                        if self.show_new_folder {
+                            Window::new("New folder").show(ctx, |ui| {
+                                ui.label("Enter folder name:");
+                                ui.add_space(5.0);
+                                ui.text_edit_singleline(&mut self.folder_name);
+                                ui.add_space(5.0);
+                                ui.horizontal(|ui: &mut Ui| {
+                                    if ui.button("Create").clicked() {
+                                        ui.spinner();
+                                        let mut path = self.path.clone();
+                                        path.push(self.folder_name.clone());
+                                        match create_dir(&path) {
+                                            Ok(_) => println!("Created folder"),
+                                            Err(e) => println!("Error: {}", e),
+                                        }
+                                        self.show_new_folder = false;
+                                    };
+                                    if ui.button("Cancel").clicked() {
+                                        self.show_new_folder = false;
+                                        self.folder_name = "".to_string();
+                                    };
+                                });
+                            });
+                        };
+                        if ui.small_button("📁+").clicked() {
+                            self.show_new_folder = true;
+                        };
+
+                        if self.show_new_note {
+                            Window::new("New File").show(ctx, |ui| {
+                                ui.label("Enter file name:");
+                                ui.add_space(5.0);
+                                ui.text_edit_singleline(&mut self.note_name);
+                                ui.add_space(5.0);
+                                ui.horizontal(|ui: &mut Ui| {
+                                    if ui.button("Create").clicked() {
+                                        ui.spinner();
+                                        let mut path = self.path.clone();
+                                        path.push(self.note_name.clone());
+                                        match OpenOptions::new()
+                                            .write(true)
+                                            .create(true)
+                                            .open(&path)
+                                        {
+                                            Ok(_) => println!("Created file"),
+                                            Err(e) => println!("Error: {}", e),
+                                        }
+                                        self.show_new_note = false;
+                                    };
+                                    if ui.button("Cancel").clicked() {
+                                        self.show_new_note = false;
+                                        self.note_name = "".to_string();
+                                    };
+                                });
+                            });
+                        };
+                        if ui.small_button("📄+").clicked() {
+                            self.show_new_note = true;
+                        };
+                    });
+
+                    ui.vertical(|ui: &mut Ui| {
+                        ui.add_space(1.0);
+                        ui.separator();
+
+                        let available_notes = read_dir(&self.path).unwrap();
+                        for entry in available_notes {
+                            let entry = match entry {
+                                Ok(entry) => entry,
+                                Err(e) => {
+                                    println!("error1: {}", e);
+                                    continue;
+                                }
+                            };
+
+                            let path = entry.path();
+                            let file_name = path.file_name().unwrap().to_str().unwrap();
+                            if ui.button(file_name).clicked() {
+                                self.current_focus = path.clone();
+                                self.current_file_buffer = read_to_string(path.clone()).expect("Corrupt path");
+
+                            }; 
+                        }
+                    });
+                });
+
+                CentralPanel::default().show(ctx, |ui| {
+                    if self.current_focus == PathBuf::from("") {
+                        ui.label("Select a note to edit, or create a new one.");
+                    } else {
+                        /* self.current_file
+                            .read_to_string(&mut self.current_file_buffer)
+                            .unwrap(); */
+                        let focus_box = ui.text_edit_multiline(&mut self.current_file_buffer);
+                        
+                        ui.vertical_centered(|ui: &mut Ui| {
+                            if ui.button("Save").clicked() {
+                                ui.spinner();
+                                // save the file ethan
+                                // set_permissions(self.current_focus, Permissions::set_readonly(&mut self, false) ); // this needs to be moved outside of the update loop
+                                let mut file = File::create(self.current_focus.clone()).unwrap();
+                                file.write(self.current_file_buffer.clone().as_bytes()).unwrap();
+                                // self.validity = false; 
+                            };
+                        });
+                    }
+                });
+            };
+        }
+    }
+
+    pub fn new_session() {
+        let mut options = NativeOptions::default();
+        options.always_on_top = false; //example customisation
+                                       // options.centered = true;
+        options.resizable = true;
+        options.min_window_size = Some(egui::Vec2::new(300.0, 400.0));
+        options.decorated = true; //should eventually be false with custom bar
+                                  /* let standard_icon = IconData {
+                                      rgba: include_bytes!("assets\\icons8-note-96.rgba").to_vec(),
+                                      width: 96,
+                                      height: 96,
+                                  };
+                                  options.icon_data = Some(standard_icon);*/
+        run_native(
+            "Notes",
+            options,
+            Box::new(|cc| Box::new(ApplicationWindow::new(cc))),
+        )
+        .expect("Failed to create application window");
+    }
+}
